aaa15f84918a7264c65215961646132dae29f696
tahmid-emam <0.1tahmidemam@gmail.com>
2023-10-06 14:44:00 +0600

    mongoose validations are added

--- src/models/clientsModel.js
+++ src/models/clientsModel.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose')
 
 const MongoSchema = mongoose.Schema({
-    name: String,
-    age: Number,
-    country: String
+    name: {
+        type: String,
+        required: [true, 'please put your name'],
+        unique: true
+    },
+    age: {
+        type: Number,
+        min: 18,
+        max: [30, `your age is more than 30` ],
+        required: true,
+    },
+    country: {
+        type: String,
+        enum: {
+            values: ['bd', 'agf'],
+            message: `you should be from bd or afg not from {VALUE}`
+        }
+    }
 })
 
 const MongoModel = mongoose.model('clients', MongoSchema)
